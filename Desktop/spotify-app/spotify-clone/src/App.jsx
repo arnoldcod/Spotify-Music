6ce994a87256567fbd45937b2f56e083eedc9aed
Arnold Uwamwezi <arnoldcod@Arnolds-MacBook-Pro.local>
2024-08-09 02:12:18 +0300

    add sidebar component

--- Desktop/spotify-app/spotify-clone/src/App.jsx
+++ Desktop/spotify-app/spotify-clone/src/App.jsx
@@ -1,8 +1,16 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { Sidebar } from './components/Sidebar'
+import Player from './components/Player'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='h-screen bg-black'>
+      <div className='h-[90% flex'>
+        <Sidebar />
+      </div>
+        <Player />
+    </div>
   )
 }
 
---
+++ Desktop/spotify-app/spotify-clone/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/App.jsx",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
