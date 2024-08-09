47df3ffe71f10219aa663dbbde09c9687dd70a77
Arnold Uwamwezi <arnoldcod@Arnolds-MacBook-Pro.local>
2024-08-10 01:23:24 +0300

    add context

--- Desktop/spotify-app/spotify-clone/src/App.jsx
+++ Desktop/spotify-app/spotify-clone/src/App.jsx
@@ -1,10 +1,15 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useContext } from 'react'
 import { Sidebar } from './components/Sidebar'
 import Player from './components/Player'
 import Display from './components/Display'
+import { PlayerContext } from './context/PlayerContext'
 
 const App = () => {
+
+  const {audioRef, track} = useContext(PlayerContext);
+
+
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90% flex'>
@@ -12,6 +17,7 @@ const App = () => {
         <Display />
       </div>
         <Player />
+        <audio ref={audioRef} src={track.file} preload='auto'> </audio>
     </div>
   )
 }
