d75061ccdee6c04c0f1c5a724bd928f8d31d9b1b
Arnold Uwamwezi <arnoldcod@Arnolds-MacBook-Pro.local>
2024-08-10 22:14:39 +0300

    add middleware, controllers and routes

--- Desktop/spotify-app/spotify-backend/server.js
+++ Desktop/spotify-app/spotify-backend/server.js
@@ -3,12 +3,16 @@ import cors from 'cors';
 import 'dotenv/config';
 import songRouter from './src/routes/songRoute.js';
 import connectDB from './src/config/mongodb.js';
+import connectCloudinary from './src/config/cloudinary.js';
 
 
 //app config
 const app = express();
 const port = process.env.PORT || 7000;
 connectDB();
+connectCloudinary();
+
+
 
 // middlewares
 app.use(express.json());
