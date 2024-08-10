920a523c2dc947811de4de36e3ebe7f225c8f70a
Arnold Uwamwezi <arnoldcod@Arnolds-MacBook-Pro.local>
2024-08-10 16:06:53 +0300

    set up server file

---
+++ Desktop/spotify-app/spotify-backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import songRouter from './src/routes/songRoute.js';
+import connectDB from './src/config/mongodb.js';
+
+
+//app config
+const app = express();
+const port = process.env.PORT || 7000;
+connectDB();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+
+//initialize routes
+app.use("/api/song", songRouter);
+
+app.get('/', (req, res)=> res.send("Api Working"))
+
+app.listen(port, ()=> console.log(`server listening on ${port}`));
