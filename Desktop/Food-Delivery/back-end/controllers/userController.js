0eebe899a59bd1b69d34eebcaacde75a55bfc0ff
Arnold Uwamwezi <arnoldcod@Arnolds-MacBook-Pro.local>
2024-08-27 15:04:34 +0300

    created user login api successfully

--- Desktop/Food-Delivery/back-end/controllers/userController.js
+++ Desktop/Food-Delivery/back-end/controllers/userController.js
@@ -4,10 +4,27 @@ import bcrypt from "bcrypt";
 import validator from "validator";
 
 
-
-
 //login user
 const loginUser = async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        const user = await userModel.findOne({email});
+            if (!user) {
+                return res.json({success: false, message: 'User not found'});
+            }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return res.json({success: false, message: 'Incorrect password'});
+            }
+      
+         const token = createToken(user._id);
+         res.json({success: true, token});
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: 'Error'});          
+    }
     
 }
 
