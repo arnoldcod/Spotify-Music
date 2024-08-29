025abb7d4db1be1d4d118356b83134815486cf50
Arnold Uwamwezi <arnoldcod@Arnolds-MacBook-Pro.local>
2024-08-29 16:21:18 +0300

    created status update on orders in admin

--- Desktop/Food-Delivery/back-end/server.js
+++ Desktop/Food-Delivery/back-end/server.js
@@ -5,6 +5,7 @@ import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
 import 'dotenv/config'
 import cartRouter from './routes/cartRoute.js';
+import orderRouter from './routes/orderRoute.js';
 
 
 
@@ -24,8 +25,9 @@ connectDB();
 //api endpoint
 app.use('/api/food', foodRouter);
 app.use('/images', express.static('uploads'));
-app.use('/api/user', userRouter)
+app.use('/api/user', userRouter);
 app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
 
 
 
