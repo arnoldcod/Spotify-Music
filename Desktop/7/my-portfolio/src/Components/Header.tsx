592e514ac5b444ae70b11c2246e68baa06b0c534
Arnold Uwamwezi <arnoldcod@Arnolds-MacBook-Pro.local>
2024-08-29 22:38:12 +0300

    added header component

--- Desktop/7/my-portfolio/src/Components/Header.tsx
+++ Desktop/7/my-portfolio/src/Components/Header.tsx
@@ -1,10 +1,11 @@
+import { IconHexagonLetterA } from "@tabler/icons-react";
 
 
 const Header = ()=> {
     return (
         <nav className="flex bg-bgColor h-[15vh] px-10 dm-mono-regular justify-between items-center">
-            <h1 className="text-primaryColor">Portfolio</h1>
-            <div className="flex gap-4">
+            <IconHexagonLetterA size={60} color="#64ffda" stroke={1.25} />
+            <div className="flex gap-8 md-mx:hidden">
                 <a className="text-textColor hover:text-primaryColor" href="#about" >About </a>
                 <a className="text-textColor hover:text-primaryColor" href="#work" >Work </a>
                 <a className="text-textColor hover:text-primaryColor"  href="#experience" >Experiences </a>
