ca6132668f8415a71b41d06b8d56025a5070cc1a
Arnold Uwamwezi <arnoldcod@Arnolds-MacBook-Pro.local>
2024-08-30 20:15:26 +0300

    added header and sidebar components

--- Desktop/7/my-portfolio/src/Components/Header.tsx
+++ Desktop/7/my-portfolio/src/Components/Header.tsx
@@ -1,19 +1,32 @@
 import { IconHexagonLetterA } from "@tabler/icons-react";
+import SideBar from "./SideBar";
 
+const links = ["About", "Work", "Experience", "Skills", "Contact"];
 
-const Header = ()=> {
-    return (
-        <nav className="flex bg-bgColor h-[15vh] px-10 dm-mono-regular justify-between items-center">
-            <IconHexagonLetterA size={60} color="#64ffda" stroke={1.25} />
-            <div className="flex gap-8 md-mx:hidden">
-                <a className="text-textColor hover:text-primaryColor" href="#about" >About </a>
-                <a className="text-textColor hover:text-primaryColor" href="#work" >Work </a>
-                <a className="text-textColor hover:text-primaryColor"  href="#experience" >Experiences </a>
-                <a className="text-textColor hover:text-primaryColor"  href="#skills" >Skills </a>
-                <a className="text-textColor hover:text-primaryColor"  href="#contact" >Contact </a>
-            </div>
-        </nav>
-    )
-}
+const navLinks = (col:Boolean) => {
+  return links.map((link, index) => (
+    <a
+      key={index}
+      className={`${
+        col ? "flex flex-col items-center" : ""
+      } text-textColor text-lg font-mono hover:text-primaryColor`}
+      href={`#${link}`}
+    >
+      <span className="text-primaryColor">0{index + 1}. </span>
+      {link}
+    </a>
+  ));
+};
+
+const Header = () => {
+  return (
+    <nav className="flex bg-bgColor h-[18vh] px-10 justify-between items-center">
+      <IconHexagonLetterA className="z-10" size={60} color="#64ffda" stroke={1.25} />
+      <div className="md:flex gap-8 hidden">{navLinks(false)}</div>
+      <SideBar />
+    </nav>
+  );
+};
 
 export default Header;
+export { navLinks };
---
+++ Desktop/7/my-portfolio/src/Components/SideBar.tsx
@@ -0,0 +1,30 @@
+import { useDisclosure } from '@mantine/hooks';
+import { Drawer,  Burger } from '@mantine/core';
+import { IconHexagonLetterA, IconX } from '@tabler/icons-react';
+import { navLinks } from './Header';
+
+
+const SideBar=()=> {
+  const [opened, { toggle }] = useDisclosure(false);
+
+
+  return (
+    <>
+   
+
+      <Drawer.Root className='md:hidden !-z-10' position='right' opened={opened} onClose={toggle}    size="50vw"  >
+        <Drawer.Overlay className='!-z-0' style={{backgroundOpacity:0.5, blur:4}} />
+        <Drawer.Content className='!-z-0' bg='#0a192f' >
+          <Drawer.Body className='mt-20 flex flex-col' bg='#0a192f'>
+             {navLinks(true)}
+            </Drawer.Body>
+        </Drawer.Content>
+      </Drawer.Root>
+
+      
+      <Burger className='md:!hidden !z-10' size='lg'  color='#64ffda'   opened={opened} onClick={toggle}  />;
+
+    </>
+  );
+}
+export default SideBar;
