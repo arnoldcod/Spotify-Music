fcd5e95f49918dead38175c54f6227dec54ef162
Arnold Uwamwezi <arnoldcod@Arnolds-MacBook-Pro.local>
2024-09-14 00:16:09 +0300

    added a listalbum page

--- Desktop/spotify-app/spotify-admin/src/pages/ListAlbum.jsx
+++ Desktop/spotify-app/spotify-admin/src/pages/ListAlbum.jsx
@@ -1,11 +1,34 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { url } from '../App';
 
 const ListAlbum = () => {
+
+  const [data, setData] = useState([]);
+
+  const fetchAlbums = async ()=>{
+
+    try {
+      const response = await axios.get(`${url}/api/album/list`);
+
+      if (response.data.success) {
+        setData(response.data.albums);
+      }
+    } catch (error) {
+      console.error('Error fetching albums:', error);
+      toast.error('Error fetching albums');  // Display error message    
+    }
+  }
+
+  useEffect(() => {
+    fetchAlbums();
+  }, [])
+
   return (
     <div>
-        list album
-    </div>
+       
+    </div> 
   )
 }
 
