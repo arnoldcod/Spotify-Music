d3ee0204b4ae872692a3612bf91b6818d9e6fa9f
Arnold Uwamwezi <arnoldcod@Arnolds-MacBook-Pro.local>
2024-09-14 00:30:26 +0300

    modified some directories

--- Desktop/Food-Delivery/back-end/config/db.js
+++
@@ -1,5 +0,0 @@
-import { mongoose } from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://arnoldcod:arnoldRutherfordpy5@cluster0.crbur.mongodb.net/Food-delivery').then(()=> console.log('DB Connected'))
-}
