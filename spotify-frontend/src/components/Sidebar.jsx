6fd1441ea9c02f408885cfef69d872b8fe416d5d
Arnold Uwamwezi <arnolduwamz7@gmail.com>
2024-09-14 23:22:15 +0300

    Update Sidebar.jsx
    spotify-frontend

--- spotify-frontend/src/components/Sidebar.jsx
+++ spotify-frontend/src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ export const Sidebar = () => {
     const navigate =  useNavigate();
 
   return (
-    <div className='w-[20%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
+    <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
         <div className='bd-[#121212] h-[15%] rounded flex flex-col justify-around'>
             <div onClick={()=> navigate('/')} className='flex items-center gap-3 pl-8 cursor-pointer'>
                 <img className='w-6' src={assets.home_icon} alt="" />
