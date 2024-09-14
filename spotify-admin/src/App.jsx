458441a280de052daf71f2644fad22a3a5602ea1
Arnold Uwamwezi <arnolduwamz7@gmail.com>
2024-09-14 23:05:08 +0300

    Update App.jsx
    spotify-backend url

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -10,7 +10,7 @@ import ListAlbum from './pages/listAlbum';
 import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 
-export const url = 'http://localhost:7000'
+export const url = 'https://spotify-backend-3w2e.onrender.com'
 
 
 const App = () => {
